84a70b3ea666dbd184fb067522fc5a1eaa741066
Jeff Lee <somejeff@gmail.com>
2022-01-28 11:31:07 -0500

    Block access to host unless owner

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Game from '../views/Game.vue'
+import { } from "@/firebaseConfig";
+import { getDatabase, ref, get } from "firebase/database";
+import { getAuth, signInAnonymously } from "firebase/auth";
 
 const routes = [
   {
@@ -16,7 +19,13 @@ const routes = [
   {
     path: '/:gameid/host',
     name: 'Host',
-    component: () => import(/* webpackChunkName: "host" */ '../views/Host.vue')
+    component: () => import(/* webpackChunkName: "host" */ '../views/Host.vue'),
+    beforeEnter: async (to, from, next) => {
+      const auth = getAuth();
+      await signInAnonymously(auth);
+      const owner = await get(ref(getDatabase(), `games/${to.params.gameid}/details/owner`));
+      next(owner.val()===auth.currentUser.uid);
+    }
   }
 ]
 
