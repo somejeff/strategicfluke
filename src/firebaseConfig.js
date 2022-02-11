4fd4ccb4ce3254ef0914ad55254d39cfcc743a46
Jeff Lee <somejeff@gmail.com>
2022-02-10 22:03:04 -0500

    #2 refactored to use secrets, maybe?

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
-const config = require('@/firebaseConfig.json');
-const firebaseApp = initializeApp(config);
+const firebaseApp = initializeApp({
+    "apiKey": process.env.VUE_APP_FIREBASE_API_KEY,
+    "authDomain": "strategicfluke.firebaseapp.com",
+    "projectId": "strategicfluke",
+    "storageBucket": "strategicfluke.appspot.com",
+    "messagingSenderId": "742807218245",
+    "appId": "1:742807218245:web:adbed60ad857cc1e3c1b2c",
+    "measurementId": "G-LVY276N72Y"
+});
 const auth = getAuth(firebaseApp);
 const db = getDatabase();
 const fb = { auth , db }
