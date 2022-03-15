f924727862c31c2f548614a52302647cfa5d00d0
Jeff Lee <somejeff@gmail.com>
2022-03-15 11:01:17 -0400

    Added app-check (Recaptcha)

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
+const { initializeAppCheck, ReCaptchaV3Provider } = require("firebase/app-check");
 const firebaseApp = initializeApp({
     "apiKey": process.env.VUE_APP_FIREBASE_API_KEY,
     "authDomain": "strategicfluke.firebaseapp.com",
@@ -10,7 +11,11 @@ const firebaseApp = initializeApp({
     "appId": "1:742807218245:web:adbed60ad857cc1e3c1b2c",
     "measurementId": "G-LVY276N72Y"
 });
+initializeAppCheck(firebaseApp, {
+    provider: new ReCaptchaV3Provider(process.env.VUE_APP_FIREBASE_RECAPTCHA_KEY),
+    isTokenAutoRefreshEnabled: true
+});
 const auth = getAuth(firebaseApp);
 const db = getDatabase();
-const fb = { auth , db }
+const fb = { auth, db }
 export default fb;
